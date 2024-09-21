feb466b2f3b9f21005ea29a45c30828fef45a72d
marbarre <martimfreitas@hotmail.com>
2024-09-21 02:17:19 +0100

    c01 ex01 done

--- c01/ex01/ft_ultimate_ft.c
+++ c01/ex01/ft_ultimate_ft.c
@@ -1,28 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marbarre <marbarre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/20 23:33:36 by marbarre          #+#    #+#             */
-/*   Updated: 2024/09/21 00:21:29 by marbarre         ###   ########.fr       */
+/*   Created: 2024/09/21 00:22:12 by marbarre          #+#    #+#             */
+/*   Updated: 2024/09/21 02:16:31 by marbarre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 
-void	ft_ft(int *nbr)
+void	ft_ultimate_ft(int *********nbr)
 {
-	*nbr = 42;
-//	printf("%d\n", *nbr);
+	*********nbr = 42;
 }
 
-/*int	main(void)
+int	main(void)
 {
-	int	num;
+	int	input;
+	int	*p1;
+	int	**p2;
+	int	***p3;
+	int	****p4;
+	int	*****p5;
+	int	******p6;
+	int	*******p7;
+	int	********p8;
+	int	*********p9;
 
-	num = 1492;
-	ft_ft(&num);
+	input = 875;
+	p1 = &input;
+	p2 = &p1;
+	p3 = &p2;
+	p4 = &p3;
+	p5 = &p4;
+	p6 = &p5;
+	p7 = &p6;
+	p8 = &p7;
+	p9 = &p8;
+	ft_ultimate_ft(p9);
+	printf("Nem Value: %d\n", input);
 	return (0);
-}*/
+}
