0929ecf584d0bd5e0c691300bb0d6141498ee251
Martim Jose Barreto Godinho Borges De Freitas <marbarre@c1r2s6.42lisboa.com>
2024-09-22 14:42:26 +0100

    worked on ex08

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -6,32 +6,39 @@
 /*   By: marbarre <marbarre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 01:50:17 by marbarre          #+#    #+#             */
-/*   Updated: 2024/09/22 02:34:34 by marbarre         ###   ########.fr       */
+/*   Updated: 2024/09/22 14:41:12 by marbarre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <limits.h>
+#include <stdio.h>
 
 void    ft_sort_int_tab(int *tab, int size)
 {
 	int i;
     int i2;
-	int temp_arr[size];
+	int temp;
 
 	i = 0;
     i2 = 0;
-	while (i < size) // populate temp_array with the highest value
+    while (i < size - 1)
     {
-        temp_arr[i] = tab[i];
-        if (tab[i] < temp_arr[i])
-            tab[i] = INT_MAX;
-            i++;
-    }
-    while (i2 < size)
-    {
-        tab[i2] = temp_arr[size - 1 - i2]; // repopulate tab
+        while (i2 < size - 1 - i)
+        {
+            if (tab[i2] > tab[i2 + 1])
+            {
+                temp = tab[i2];
+                tab[i2] = tab[i2 + 1];
+                tab[i2 + 1] = temp;
+            }
+		if (tab[i2] <= tab[i2 + 1])
+        printf("%d", tab[i2]);
+        printf(", ");
         i2++;
+        }
+    i++;
     }
+    printf("%d", tab[i2]);
+    printf("\n");
 }
 
 int main(void)
