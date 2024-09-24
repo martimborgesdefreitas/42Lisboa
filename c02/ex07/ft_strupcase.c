6fdde5f5fff7a22420a1cf98e56f9ce7136eae7b
Martim Jose Barreto Godinho Borges De Freitas <marbarre@c1r1s12.42lisboa.com>
2024-09-24 23:40:18 +0100

    finished c02 ex07

--- c02/ex07/ft_strupcase.c
+++ c02/ex07/ft_strupcase.c
@@ -6,14 +6,36 @@
 /*   By: marbarre <marbarre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 22:57:51 by marbarre          #+#    #+#             */
-/*   Updated: 2024/09/24 23:00:03 by marbarre         ###   ########.fr       */
+/*   Updated: 2024/09/24 23:39:42 by marbarre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *ft_strupcase(char *str)
+//#include <stdio.h>
+
+char	*ft_strupcase(char *str)
 {
-    int	i;
+	int	i;
 
 	i = 0;
-	
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
 }
+
+/*int	main(void)
+{
+	char	str[4];
+
+	str[0] = 'h';
+	str[1] = 'a';
+	str[2] = 'A';
+	str[3] = '\0';
+	
+	ft_strupcase(str);
+	printf("%s", ft_strupcase(str));
+	return(0);
+}*/
