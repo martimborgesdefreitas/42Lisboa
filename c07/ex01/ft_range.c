847fca15bad75d7b5de8837b4daa72fc4e4e11fc
Martim Jose Barreto Godinho Borges De Freitas <marbarre@c1r3s8.42lisboa.com>
2024-09-30 21:37:45 +0100

    c07 ex01 finished

---
+++ c07/ex01/ft_range.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marbarre <marbarre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 20:49:28 by marbarre          #+#    #+#             */
+/*   Updated: 2024/09/30 21:37:24 by marbarre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*arr;
+	int	i;
+	int	size;
+
+	size = max - min;
+	if (min >= max)
+		return (NULL);
+	arr = malloc((max - min) * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = min;
+		min++;
+		i++;
+	}
+	return (arr);
+}
+
+/*int	main(void)
+{
+	int	min;
+	int	max;
+	int	*arr;
+	int	i;
+
+	min = -168;
+	max = 574;
+	arr = ft_range(min, max);
+	i = 0;
+	while (i < max - min)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\n");
+	free(arr);
+	return (0);
+}*/
