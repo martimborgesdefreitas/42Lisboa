e54d924cc0a23917570500b7d546b9b5a84e9deb
Martim Jose Barreto Godinho Borges De Freitas <marbarre@c1r5s8.42lisboa.com>
2024-10-01 21:16:34 +0100

    c07 ex02 finished

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: marbarre <marbarre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 21:40:03 by marbarre          #+#    #+#             */
-/*   Updated: 2024/09/30 21:40:57 by marbarre         ###   ########.fr       */
+/*   Updated: 2024/10/01 21:16:14 by marbarre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,47 @@
 
 int	ft_ultimate_range(int **range, int min, int max)
 {
-	
+	int	size;
+	int	i;
+
+	*range = malloc ((max - min) * sizeof(int));
+	if (*range == NULL)
+		return (-1);
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	i = 0;
+	size = max - min;
+	while (i < size)
+	{
+		(*range)[i] = min;
+		min++;
+		i++;
+	}
+	return (size);
 }
+
+/*int	main(void)
+{
+	int	min;
+	int	max;
+	int	*arr;
+	int	i;
+	int	size;
+
+	min = -68;
+	max = 74;
+	size = ft_ultimate_range(&arr, min, max);
+	i = 0;
+	while (i < max - min)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+	printf("\n");
+	printf("size of the array is: %d", size);
+	free(arr);
+	return (0);
+}*/
